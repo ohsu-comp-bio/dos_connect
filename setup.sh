793fbf9743f89bba5c2defca1468850946898061
Brian Walsh <brian@bwalsh.com>
2017-10-06 20:34:21 +0000

    remove schema-registry, add excercise of kafka-connect messages from curl

--- setup.sh
+++ setup.sh
@@ -100,7 +100,7 @@ docker run \
  --net=host \
  --rm \
  confluentinc/cp-kafka:3.3.0 \
- kafka-console-consumer --bootstrap-server localhost:29092 --topic directory_topic --new-consumer --from-beginning --max-messages 10
+ kafka-console-consumer --bootstrap-server localhost:29092 --topic directory_topic --new-consumer --from-beginning --max-messages 4
 
 
 echo "## checking elasticsearch-sink worker in kafka worker config..."
@@ -110,10 +110,10 @@ if [ $? -eq 0 ]; then
 else
   echo "### elasticsearch-sink not found creating it..."
   echo "### will read directory entries of from topic 'directory_topic' and write to elastic index 'directory_topic' ..."
-  curl -X POST -H "Content-Type: application/json" --data '{"name":"elasticsearch-sink","config":{"connector.class":"io.confluent.connect.elasticsearch.ElasticsearchSinkConnector","type.name":"directory","tasks.max":"1","topics":"directory_topic","key.ignore":"true","schema.ignore":"true","connection.url":"http://localhost:9200"}}'  http://localhost:28082/connectors
-  http://localhost:28082/connectors 
+  curl --silent -X POST -H "Content-Type: application/json" --data '{"name":"elasticsearch-sink","config":{"connector.class":"io.confluent.connect.elasticsearch.ElasticsearchSinkConnector","type.name":"directory","tasks.max":"1","topics":"directory_topic","key.ignore":"true","schema.ignore":"true","connection.url":"http://localhost:9200"}}'  http://localhost:28082/connectors > /dev/null
+  printf '.'
   sleep 5
-  curl --fail --silent  localhost:28082/connectors/elasticsearch-sink/status  
+  curl --fail --silent  localhost:28082/connectors/elasticsearch-sink/status  > /dev/null
   if [ $? -eq 0 ]; then
     echo "elasticsearch-sink found OK"
   else
@@ -122,5 +122,73 @@ else
   fi
 fi
 
+echo "## wait for elastic index to create ..."
+until $(curl --output /dev/null --silent  --fail localhost:9200/directory_topic); do
+    printf '.'
+    sleep 5
+done
+echo "elastic index created OK"
+
 echo '## list directory elements from elasticsearch...'
-curl --silent  localhost:9200/directory_topic/_search | jq -r  .hits.hits[]._source.path | uniq
+curl --silent  localhost:9200/directory_topic/_search | jq -r  .hits.hits[]._source.path | sort |  uniq
+
+# create an image to build python consumer
+echo "## checking for python consumer image..."
+[ ! -z $(docker images -q python-kafka-consumer:latest) ] || docker build -t python-kafka-consumer -f python-kafka-consumer.dockerfile .
+if [ -z $(docker images -q python-kafka-consumer:latest) ]; then
+  echo $?
+  echo "python-kafka-consumer does not exist FAIL"; exit 1
+fi
+echo "python-kafka-consumer does exists OK"
+
+echo "## consuming directory_topic from python 'native' client..."
+docker run  --net=host --rm -it -v $(pwd)/directory_consumer.py:/directory_consumer.py python-kafka-consumer python /directory_consumer.py
+if [ $? -eq 0 ]; then
+  echo "directory_consumer.py OK"
+else
+  echo "directory_consumer.py FAIL"
+  exit 1
+fi
+
+echo "## consuming directory_topic meta data from REST interface ..."
+curl --silent localhost:8082/topics | grep directory_topic > /dev/null
+if [ $? -eq 0 ]; then
+  echo "REST metadata OK"
+else
+  echo "REST metadata FAIL"
+  exit 1
+fi
+
+
+# Create a consumer for JSON data, starting at the beginning of the topic's
+# log and subscribe to a topic. Then consume some data using the base URL in the first response.
+# Finally, close the consumer2 with a DELETE to make it leave the group and clean up
+# its resources.
+echo "## setting up REST consumer..."
+curl --silent  -X POST -H "Content-Type: application/vnd.kafka.v2+json" \
+      --data '{"name": "my_consumer_instance", "format": "json", "auto.offset.reset": "earliest"}' \
+      http://localhost:8082/consumers/my_json_consumer > /dev/null
+
+echo "## setting up REST consumer subscription to directory_topic..."
+curl --silent -X POST -H "Content-Type: application/vnd.kafka.v2+json" --data '{"topics":["directory_topic"]}' \
+ http://localhost:8082/consumers/my_json_consumer/instances/my_consumer_instance/subscription
+ # No content in response
+
+
+echo "## get REST consumer directory_topic messages..."
+while :
+do
+  curl --silent -X GET -H "Accept: application/vnd.kafka.json.v2+json" http://localhost:8082/consumers/my_json_consumer/instances/my_consumer_instance/records | grep directory_topic
+  if [ $? -eq 0 ]
+  then
+  	break            #Abandon the loop.
+  fi
+  printf '.'
+  sleep 5
+done
+
+
+echo "## delete REST consumer..."
+curl -X DELETE -H "Content-Type: application/vnd.kafka.v2+json" \
+       http://localhost:8082/consumers/my_json_consumer/instances/my_consumer_instance
+  # No content in response
