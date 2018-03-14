f0a64bc1cf908619636816567754c8790b22d4f6
Brian Walsh <brian@bwalsh.com>
2018-03-14 11:45:49 -0700

    save bucketname in state pickle filename

--- dos_connect/apps/inventory/__init__.py
+++ dos_connect/apps/inventory/__init__.py
@@ -6,18 +6,18 @@ scrape cloud side buckets
 import pickle
 
 
-def get_offset():
+def get_offset(bucket_name):
     """ return offset dict """
     offset = None
     try:
-        with open('offset.pickle') as data_file:
+        with open('offset.{}.pickle'.format(bucket_name)) as data_file:
             offset = pickle.load(data_file)
     except Exception as e:
         pass
     return offset
 
 
-def save_offset(offset):
+def save_offset(offset, bucket_name):
     """ save offset dict """
-    with open('offset.pickle', 'w') as data_file:
+    with open('offset.{}.pickle'.format(bucket_name), 'w') as data_file:
         pickle.dump(offset, data_file)
